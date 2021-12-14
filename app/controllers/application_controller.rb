a731859ef232c8cbaa8b9987b8bf83858fad1fed
Veronika <veronika.pilipenko@gmail.com>
2021-12-14 16:37:41 -0500

    fixed name error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,53 @@
 class ApplicationController < ActionController::Base
+
+    before_action :underscore_params!
+    def underscore_params!
+    underscore_hash = -> (hash) do
+        hash.transform_keys!(&:underscore)
+        hash.each do |key, value|
+        if value.is_a?(ActionController::Parameters)
+            underscore_hash.call(value)
+        elsif value.is_a?(Array)
+            value.each do |item|
+            next unless item.is_a?(ActionController::Parameters)
+            underscore_hash.call(item)
+            end
+        end
+        end
+    end
+    underscore_hash.call(params)
+    end
+
+    helper_method :current_user, :logged_in?
+
+    #CRLLL
+
+    def current_user
+        return nil if session[:session_token].nil?
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { base: ['invalid credentials'] }, status: 401
+        end
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logout!
+        current_user.reset_session_token! if logged_in?
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
 end
+
+
