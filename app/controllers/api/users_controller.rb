0626c2f4ba787ddd65f9ed7e33eaac021ed1aa53
Veronika <veronika.pilipenko@gmail.com>
2022-03-08 12:41:25 -0500

    user controller working

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,47 @@
 class Api::UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new 
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            render :show 
+        else 
+            render json: @user.errors.full_messages, status: 401
+        end 
+    end 
+
+    def show
+        @user = User.find(params[:id])
+        if @user
+            render :show
+        else 
+            render json: @user.errors.full_messages, status: 404
+        end 
+    end 
+
+    def index 
+        @users = User.all 
+        render :index 
+    end 
+
+    def destroy 
+        @user = User.find(params[:id])
+        if @user
+            @user.destroy
+            render :show
+        else 
+            render json: ['User not found']
+        end 
+    end 
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end 
+
 end
