e0a2a381c0d740eabfacdc4d8a31516228b5ff28
Veronika <veronika.pilipenko@gmail.com>
2022-03-22 21:59:59 -0400

    react router works

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,20 +5,24 @@ import {
     Redirect,
     Switch,
     Link,
-    HashRouter
 } from 'react-router-dom';
 
 import { ProtectedRoute, AuthRoute } from '../util/route_util';
 import NavBar from './nav_bar/nav_bar';
 // import SessionForm from './session_form/session_forms';
 import Home_page from './home_page/Home_page'
+import ContactForm from './contact_form/contact_form'
 
 const App = () => (
     <div className="outer-container">
         <div className="inner-container">
             <NavBar/>
+            <Switch>
+                <Route exact path="/" component={Home_page} />
+                <Route exact path="/contact" component={ContactForm} />
+            </Switch>
             {/* <SessionForm /> */}
-            <Home_page /> 
+            {/* <Home_page />  */}
         </div>
     </div>
 
