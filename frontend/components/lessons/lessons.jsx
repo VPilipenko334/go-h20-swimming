3b3125ffa5cb28afbcbd370c33e61833f2e15afd
Veronika <veronika.pilipenko@gmail.com>
2022-03-24 16:50:09 -0400

    adding static pages

--- frontend/components/lessons/lessons.jsx
+++ frontend/components/lessons/lessons.jsx
@@ -4,7 +4,82 @@ const Lessons = () => {
 
   return (
       <div>
-          <h1>Hi I am lessons</h1>
+        
+      <h1>Baby Otters</h1> 
+      <br/>
+      <p>Our Baby Otters class encourages parents to take their 
+      little ones in the water. Our skillful instructors will 
+      guide the process of floating, gliding back and forth and 
+      singing songs to the our little swimming friends. While this 
+      class does focus on having your baby in the water, it is also 
+      a learning experience for the parent on aquatic safety around 
+      your child.</p>
+      <br/>
+      <p>This class is open to babies 6- 18 months of age.</p>
+
+      <br/>
+      <br/>
+
+      <h1>Beginner 1: Guppies</h1>
+      <p>Our Beginner 1 class is designed to teach rudimentary
+      skills to our swimmers. Blowing bubbles, floating on their 
+      fronts and back and gliding will be introduced by our instructors.
+        <br />
+      This class is also going to help our little swimmers before
+      more comfortable and confident in a pool environment— learning
+      safety skills such as spotting the lifeguard, climbing in and 
+      out of the pool and general safety.
+        <br />
+      This class is open to all ages</p>
+
+      <br />
+      <br />
+
+      <h1>Beginner 2: Minnows</h1>
+      <p>Our Beginner 2 class focuses more on floating, gliding and kicking. 
+      This class is going to help our little swimmers become more confident
+      in the pool, as well as introduce the technique of putting their 
+      face in the pool and holding their breath under the water. Safety 
+      techniques will be enforced throughout the course of the class.
+      <br />
+        This class is open to all ages. </p>
+
+      <br />
+      <br />
+
+      <h1>Advanced 1: Dolphins</h1>
+      <p>Go H2O’s Advanced 1 class will cater to those that have a 
+      little more swimming experience. Our instructors will guide 
+      our swimmers on how to do a proper streamline, pointed-toe kicks, 
+      and independent floats on their stomach and backs. Underwater
+      discovery games will be played at the end of the class to ensure 
+      the swimmers are comfortable and confident in the pool!
+      <br />
+      This class is open to all ages.</p>
+
+      <br />
+      <br />
+
+      <h1>Advanced 2: Swim Team Prep</h1>
+      <p>Our Advanced 2 class will encourage those that are more
+      advanced in the swimming world. This class will have sets 
+      that swimmers can do to build stamina, endurance and strength. 
+      Strokes such as backstroke, breaststroke and butterfly will be 
+      introduced and practiced.
+      <br />
+      This class is open to all ages</p>
+
+      <br />
+      <br />
+
+      <h1>Adult Lessons</h1>
+      <p>Our adult swim lesson program will cater to those that are 
+      learning to swim as adults. This program will cater to all levels. 
+      Lessons will consist of underwater exploration to pushing off the 
+      wall to learning the basis of the four strokes.
+      <br />
+      This class is open to ages 18+.</p>
+
       </div>
   )
 }
